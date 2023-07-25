674b30caf1cf3b7d018197d3585fbb225f87e9ba
Mr.Eli <eliseo.torres95@gmail.com>
2023-07-25 00:40:04 -0500

    update hv

--- js/main.js
+++ js/main.js
@@ -1,34 +1,44 @@
 const menu = document.querySelector(".menu");
 const change_icon_menu = document.querySelector(".icon_dark_mode");
 const body = document.body;
+const button_cv = document.querySelector("#cv");
+const path_cv = "./docs/CV_ELISEO_TORRES.pdf";
+const menu_over = document.querySelector(".menu");
+const menu_tag_show = document.querySelector(".list_menu");
+const menu_over_hidden = document.querySelector(".icon_close");
 
-let dark = Boolean(localStorage.getItem("DarkMode"));
-
+let dark = JSON.parse(localStorage.getItem("DarkMode"));
+console.log(dark);
 if (dark) {
   body.classList.add("dark");
   change_icon_menu.classList.add("fa-sun");
   change_icon_menu.classList.remove("fa-moon");
 } 
+else{
+  body.classList.remove("dark");
+  
+}
 menu.addEventListener("click", function (e) {
     if (e.target.classList.contains("icon_dark_mode")) {
+      
       if (body.classList.contains("dark")) {
         body.classList.remove("dark");
         change_icon_menu.classList.add("fa-moon");
         change_icon_menu.classList.remove("fa-sun");
         localStorage.setItem("DarkMode", "false");
+        console.log(dark);
       } else {
         body.classList.add("dark");
         change_icon_menu.classList.add("fa-sun");
         change_icon_menu.classList.remove("fa-moon");
         localStorage.setItem("DarkMode", "true");
+        console.log(dark);
       }
     }
   });
 
 
-const menu_over = document.querySelector(".menu");
-const menu_tag_show = document.querySelector(".list_menu");
-const menu_over_hidden = document.querySelector(".icon_close");
+
 
 menu_over.addEventListener("click", function (e) {
   if (e.target.classList.contains("icon_menu")) {
@@ -40,4 +50,8 @@ menu_over_hidden.addEventListener("click", function (e) {
     menu_tag_show.classList.toggle("list_menu_show");
   }
 });
+
+button_cv.addEventListener("click",function (e) {
+  window.open(path_cv, "_blank");
+})
 
